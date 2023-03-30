78a98a84ae2f32f43d95911c201ab3a6b7e1319e
genaldpaulo <114406756+genaldpaulo@users.noreply.github.com>
2023-03-30 19:02:59 +0800

    uploading genmovies

---
+++ app/[movie]/page.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image"
+
+export async function generateStaticParams(){
+    const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+    const res = await data.json()
+    return res.results.map((movie) => ({
+        movie: toString(movie.id),
+    }))
+}
+
+export default async function MovieDetail({ params }) {
+   
+    const { movie } = params
+    const imagePath = "https://image.tmdb.org/t/p/original"
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
+
+    const res = await data.json()
+    // console.log(res)
+    return (
+        <div>
+            <div>
+                <h2 className="text-2xl">{res.title}</h2>
+                <h2>{res.release_date}</h2>
+                <h2> Runtime: {res.runtime} minutes</h2>
+                <h2 className="text-sm text-white bg-green-600 inline-block my-2 py-2 px-4 rounded"> {res.status} </h2>
+                <Image className="my-12 w-original" src={imagePath + res.backdrop_path}
+                    width={1000}
+                    height={1000}
+                    alt={res.title}
+                    priority
+                />
+                <p className=""> {res.overview}</p>
+            </div>
+        </div>
+    )
+}
---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <main>
+            <h1>
+                This is about page!
+            </h1>
+        </main>
+    )
+}
